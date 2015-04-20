adb834d6f9582e824512c28098249936286194ab
ejohfal <johan.falkenstrom@ericsson.com>
2015-04-20 08:44:45 +0200

    updated code with testify suite package

--- BowlingGame.go
+++ BowlingGame.go
@@ -2,49 +2,44 @@ package BowlingGame
 
 type Game struct {
 	rolls [21]int
-	currentRoll int
+	numRolls int
 }
 
-func (g *Game) Roll(pins int) {
-	g.rolls[g.currentRoll] = pins
-	g.currentRoll ++
-}
-
-func (g *Game) Score() int {
-	score := 0
-	frameIndex := 0
-	for frame := 0; frame < 10; frame ++ {
-		if g.isStrike(frameIndex) {
-			// Strike
-			score += 10 + g.strikeBonus(frameIndex)
-			frameIndex++
-		} else if g.isSpare(frameIndex) {
-			score += 10 + g.spareBonus(frameIndex)
-			frameIndex += 2
-		} else {
-			score += g.sumOfBallsInFrame(frameIndex)
-			frameIndex += 2
-		}
-	}
-	return score
-}
-
-func (g *Game) sumOfBallsInFrame(frameIndex int) int {
-	return g.rolls[frameIndex] + g.rolls[frameIndex+1]
-}
-
-func (g *Game) spareBonus(frameIndex int) int {
-	return g.rolls[frameIndex+2]
-}
-
-func (g *Game) strikeBonus(frameIndex int) int {
-	return g.rolls[frameIndex+1] + g.rolls[frameIndex+2]
+func (g *Game)Roll(pins int) {
+	g.rolls[g.numRolls] = pins
+	g.numRolls++
 }
 
 func (g *Game) isSpare(frameIndex int) bool {
 	return g.rolls[frameIndex] + g.rolls[frameIndex+1] == 10
 }
 
+func (g *Game) spareBonus(frameIndex int) int {
+	return g.rolls[frameIndex+2]
+}
+
 func (g *Game) isStrike(frameIndex int) bool {
 	return g.rolls[frameIndex] == 10
+}
+
+func (g *Game) strikeBonus(frameIndex int) int {
+	return g.rolls[frameIndex+1] + g.rolls[frameIndex+2]
+}
+
+func (g *Game)Score() int {
+	score := 0
+	frameIndex := 0
+	for i := 0; i < 10; i ++ {
+		if g.isSpare(frameIndex) {
+			score += g.rolls[frameIndex] + g.rolls[frameIndex+1] + g.spareBonus(frameIndex)
+			frameIndex += 2
+		} else if g.isStrike(frameIndex) {
+			score += g.rolls[frameIndex] + g.strikeBonus(frameIndex)
+			frameIndex += 1
+		} else {
+			score += g.rolls[frameIndex] + g.rolls[frameIndex+1]
+			frameIndex += 2
+		}
+	}
+	return score
 }
--- bowlinggame_test.go
+++ bowlinggame_test.go
@@ -3,54 +3,63 @@ package BowlingGame
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func rollMany(g *Game, n, pins int) {
-	for i := 0; i < n; i ++ {
-		g.Roll(pins)
+type GameTestSuite struct {
+	suite.Suite
+	g Game
+}
+
+func (suite *GameTestSuite) SetupTest() {
+	suite.g = Game{}
+}
+
+func (suite *GameTestSuite) rollMany(rolls, pins int) {
+	for i := 0; i < rolls; i ++ {
+		suite.g.Roll(pins)
 	}
 }
 
-func rollSpare(g *Game) {
-	g.Roll(5)
-	g.Roll(5)
+func (suite *GameTestSuite) rollSpare() {
+	suite.g.Roll(5)
+	suite.g.Roll(5)
 }
 
-func rollStrike(g *Game) {
-	g.Roll(10)
+func (suite *GameTestSuite) rollStrike() {
+	suite.g.Roll(10)
 }
 
-func TestGutterGame(t *testing.T) {
-	g := Game{}
-	rollMany(&g, 20, 0)
-	assert.Equal(t, 0, g.Score())
+func (suite *GameTestSuite) TestGutterGame() {
+	suite.rollMany(20, 0)
+	assert.Equal(suite.T(), 0, suite.g.Score())
 }
 
-func TestAllOnes(t *testing.T) {
-	g := Game{}
-	rollMany(&g, 20, 1)
-	assert.Equal(t, 20, g.Score())
+func (suite *GameTestSuite) TestAllOnes() {
+	suite.rollMany(20, 1)
+	assert.Equal(suite.T(), 20, suite.g.Score())
 }
 
-func TestOneSpare(t *testing.T) {
-	g := Game{}
-	rollSpare(&g)
-	g.Roll(3)
-	rollMany(&g, 17, 0)
-	assert.Equal(t, 16, g.Score())
+func (suite *GameTestSuite) TestOneSpare() {
+	suite.rollSpare()
+	suite.g.Roll(3)
+	suite.rollMany(17, 0)
+	assert.Equal(suite.T(), 16, suite.g.Score())
 }
 
-func TestOneStrike(t *testing.T) {
-	g := Game{}
-	rollStrike(&g)
-	g.Roll(3)
-	g.Roll(4)
-	rollMany(&g, 16, 0)
-	assert.Equal(t, 24, g.Score())
+func (suite *GameTestSuite) TestOneStrike() {
+	suite.rollStrike()
+	suite.g.Roll(3)
+	suite.g.Roll(4)
+	suite.rollMany(16, 0)
+	assert.Equal(suite.T(), 24, suite.g.Score())
 }
 
-func TestPerfectGame(t *testing.T) {
-	g := Game{}
-	rollMany(&g, 12, 10)
-	assert.Equal(t, 300, g.Score())
+func (suite *GameTestSuite) TestPerfectGame() {
+	suite.rollMany(12, 10)
+	assert.Equal(suite.T(), 300, suite.g.Score())
+}
+
+func TestGameTestSuite(t *testing.T) {
+	suite.Run(t, new(GameTestSuite))
 }
